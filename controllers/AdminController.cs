8c47843b525d9a8c30899eab7771f6d78059a3f1
neytchi <neyton61@gmail.com>
2020-04-24 19:22:31 +0300

    INF: define controllers

---
+++ controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<dynamic> GetQuestions()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
--- controllers/QuestionController.cs
+++ controllers/QuestionController.cs
@@ -6,37 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace controllers
 {
-    [Route("api/[controller]")]
+    [Route("/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class QuestionController : ControllerBase
     {
-        // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<dynamic> GetQuestions()
         {
-            return new string[] { "value1", "value2" };
+            return null;
         }
-
-        // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
-
-        // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
-
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
-
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
