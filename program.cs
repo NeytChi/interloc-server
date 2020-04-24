c44832b846f7756ad11294213c52d92c69666e40
neytchi <neyton61@gmail.com>
2020-04-24 19:10:36 +0300

    INF: configurate project's general settings

--- program.cs
+++ program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace interloc
+namespace common
 {
     public class Program
     {
